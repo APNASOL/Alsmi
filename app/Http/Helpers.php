ff8e768090c749a9fe60ce327b09aaa453ed4c32
BILAL HUSSAIN <billhssn777@icloud.com>
2025-02-26 00:19:59 +0500

    updated

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -1,20 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Storage;
 
- 
- 
-// function get_storage_url($path)
-// {
-//     // Get base URL from config
-//     $baseUrl = config('app.url'); // This is your domain, e.g., 'http://alsmi.test'
-
-//     // Get the relative URL from storage
-//     $relativeUrl = Storage::url($path); // This will return something like '/storage/TransactionReceipts/...'
-
-//     // Combine the base URL with the relative URL
-//     return $baseUrl . $relativeUrl;
-// }
- 
 if (!function_exists('getFileUrl')) {
     function getFileUrl($file_name)
     {
